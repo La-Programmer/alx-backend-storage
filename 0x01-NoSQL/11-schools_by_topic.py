d66f5b50529ed9e04f9f9ec6afb5f5b7dcbec617
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-07-16 07:59:02 -0700

    Add task 11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python3
+"""FECTCH AND FILTER ACCORDING TO TOPICS"""
+
+import pymongo
+from typing import List
+
+
+def schools_by_topic(mongo_collection, topic) -> List:
+    """Function to fetch and filter"""
+    result_list: List = mongo_collection.find({"topics": {"$in": [topic]}})
+    return result_list
