34d1dff84a354aa429c79dde24a8ffa2fec88618
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-07-18 10:55:11 +0100

    Update made to file

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,7 @@
 
 import redis
 from typing import Union
+from typing import Callable
 from uuid import uuid4
 
 
@@ -17,3 +18,22 @@ class Cache:
         key: str = str(uuid4())
         self._redis.set(name=key, value=data)
         return key
+    
+
+    def get(self, key: str, fn=None) -> Union[str, int, bytes, float, None]:
+        """Get a value from redis and convert it"""
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """Retrieves a value from Redis as a string"""
+        return self.get(key, lambda v: v.decode('utf-8'))
+    
+
+    def get_int(self, key: str) -> int:
+        """Retrieves a value from Redis as an integer"""
+        return self.get(key, int)
