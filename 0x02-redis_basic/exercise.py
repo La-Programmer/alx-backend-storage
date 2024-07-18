c012d1eefb0a9b60dea0fcb8e6a2eb838240abe7
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-07-18 11:47:53 +0100

    Update with task 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -23,20 +23,36 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+
+def call_history(method: Callable) -> Callable:
+    """
+    Decorator to store list of inputs and outputs to a function
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper function"""
+        inputs_key: str = f'{method.__qualname__}:inputs'
+        outputs_key: str = f'{method.__qualname__}:outputs'
+        self._redis.rpush(inputs_key, str(args))
+        outputs = method(self, *args, **kwargs)
+        self._redis.rpush(outputs_key, outputs)
+        return outputs
+    return wrapper
+
+
 class Cache:
     """Create a Redis instance"""
     def __init__(self) -> None:
         self._redis: redis.Redis = redis.Redis()
-    
+
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generates a random key using uuid"""
         key: str = str(uuid4())
         self._redis.set(name=key, value=data)
         return key
 
-
-    @count_calls
     def get(self, key: str, fn=None) -> Union[str, int, bytes, float, None]:
         """Get a value from redis and convert it"""
         value = self._redis.get(key)
@@ -46,14 +62,10 @@ class Cache:
             return fn(value)
         return value
 
-
-    @count_calls
     def get_str(self, key: str) -> str:
         """Retrieves a value from Redis as a string"""
         return self.get(key, lambda v: v.decode('utf-8'))
-    
 
-    @count_calls
     def get_int(self, key: str) -> int:
         """Retrieves a value from Redis as an integer"""
         return self.get(key, int)
